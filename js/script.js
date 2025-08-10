59dba05f9102101072191d3e7d56ce1a91f4bee1
Eduardo Barros <edu.barros101@gmail.com>
2025-08-09 22:45:20 -0300

    Initial Script

--- js/script.js
+++ js/script.js
@@ -43,7 +43,8 @@ function loadMovies() {
       genero: 'Ficção Científica, Ação',
       descricao:
         'Um programador de computador descobre que a realidade é uma simulação.',
-      imagem: 'https://via.placeholder.com/150/FFFF33/000000?text=Matrix',
+      imagem:
+        'https://revistacontinente.com.br/image/view/news/image/2117/mobile',
       gostei: 0,
       naoGostei: 0,
     },
@@ -59,3 +60,123 @@ function loadMovies() {
     },
   ];
 }
+
+// Criando a função para salvar os dados no LocalStorage.
+function saveMovies(movies) {
+  localStorage.setItem('movies', JSON.stringify(movies));
+}
+
+// Carregando os dados na variável.
+let movies = loadMovies();
+
+const moviesContainer = document.getElementById('movies-container');
+const addMoviesForm = document.getElementById('add-movie-form');
+const globalPositiveVotesSpan = document.getElementById(
+  'global-positive-votes'
+);
+const globalNegativeVotesSpan = document.getElementById(
+  'global-negative-votes'
+);
+
+// Renderizando
+function renderMovie(movie) {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie-card');
+  movieCard.dataset.id = movie.id;
+
+  movieCard.innerHTML = `
+  <h3>${movie.titulo}</h3>
+  <img src="${movie.imagem}" alt="${movie.titulo}">
+  <p><strong>Gênero:</strong> ${movie.genero}</p>
+  <p><strong>Descrição:</strong> ${movie.descricao || 'NA'}</p>
+  <div class="votes">
+    <button class="vote-button like-button" data-type="like">Gostei</button>
+    <span class="positive-count">${movie.gostei}</span>
+    <button class="vote-button dislike-button" data-type="dislike">Não Gostei</button>
+    <span clarr="negative-count">${movie.naoGostei}</span>
+  `;
+
+  // Adicionando event listeners para os botões de voto.
+  movieCard
+    .querySelector('.like-button')
+    .addEventListener('click', () => handleVote(movie.id, 'like'));
+  movieCard
+    .querySelector('.dislike-button')
+    .addEventListener('click', () => handleVote(movie.id, 'dislike'));
+
+  return movieCard;
+}
+
+function renderAllMovies() {
+  moviesContainer.innerHTML = '';
+  movies.forEach((movie) => {
+    moviesContainer.appendChild(renderMovie(movie));
+  });
+
+  updateGlobalVotes();
+}
+
+function handleVote(id, type) {
+  const movieIndex = movies.findIndex((m) => m.id === id);
+  if (movieIndex > -1) {
+    if (type === 'like') {
+      movies[movieIndex].gostei++;
+    } else if (type === 'dislike') {
+      movies[movieIndex].naoGostei++;
+    }
+
+    saveMovies(movies); // Salvando os dados atualizados.
+    updateMovieCard(id); // Atualizando apenas o card do filme.
+    updateGlobalVotes();
+  }
+}
+
+function updateMovieCard(id) {
+  const movie = movies.find((m) => m.id === id);
+
+  if (movie) {
+    const movieCard = document.querySelector(`.movie-card[data-id"${id}"]`);
+    if (movieCard) {
+      movieCard.querySelector('.positive-count').textContent = movie.gostei;
+      movieCard.querySelector('.negative-count').textContent = movie.naoGostei;
+    }
+  }
+}
+
+// Atualizando os totais globais de votos
+function updateGlobalVotes() {
+  let totalPositive = 0;
+  let totalNegative = 0;
+
+  movies.forEach((movie) => {
+    totalPositive += movie.gostei;
+    totalNegative += movie.naoGostei;
+  });
+
+  globalPositiveVotesSpan.textContent = totalPositive;
+  globalNegativeVotesSpan.textContent = totalNegative;
+}
+
+// Tratando o envio do formulário de adição de filme.
+addMoviesForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const newMovie = {
+    id: String(Date.now()), // Gerando um ID único baseado no timestamp. Preciso pesquisar se há outra forma melhor.
+    titulo: document.getElementById('title').value,
+    genero: document.getElementById('genre').value,
+    imagem: document.getElementById('image').value,
+    descricao: document.getElementById('description').value,
+    gostei: 0,
+    naoGostei: 0,
+  };
+
+  movies.push(newMovie);
+  saveMovies(movies);
+  moviesContainer.appendChild(renderMovie(newMovie)); // Adicionando o novo filme à lista com o appendChild.
+  updateGlobalVotes();
+  addMoviesForm.reset(); // Para limpar o formulário.
+  alert('Filme/Série cadastrada com sucesso!');
+});
+
+document.addEventListener('DOMContentLoaded', renderAllMovies);
